97a4d8a6434ee99b2e35992c52ff1931b293cd0c
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-06 14:00:36 +0530

    Update Asssumerole.sh

--- Asssumerole.sh
+++ Asssumerole.sh
@@ -1,4 +1,4 @@
  #!/bin/sh
-aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance --output text> ram.txt
+aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance --output text> ram1.txt
 cat ram.txt
 
