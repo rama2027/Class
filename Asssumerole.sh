b4b6646b349485481237f57284135688f95fa067
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-06 13:41:16 +0530

    Update Asssumerole.sh

--- Asssumerole.sh
+++ Asssumerole.sh
@@ -1,3 +1,3 @@
-credentails = aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance
+$credentails = aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance
 echo $credentails
 
