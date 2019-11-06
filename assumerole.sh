a51931b587dca4e35bb585441b925d8bf442dd31
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-06 18:07:49 +0530

    Create assumerole.sh

---
+++ assumerole.sh
@@ -0,0 +1,8 @@
+ #!/bin/sh
+aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance --output json > Cred.json
+jq -r '.["Credentials"]' Cred.json > Cred.txt
+cat Cred.txt
+export AWS_SECRET_ACCESS_KEY=$(awk '/SecretAccessKey/ {print $2}' Cred.txt | tr -d '",{}')
+export AWS_SESSION_TOKEN=$(awk '/SessionToken/ {print $2}' Cred.txt | tr -d '",{}')
+export AWS_ACCESS_KEY_ID=$(awk '/AccessKeyId/ {print $2}' Cred.txt | tr -d '",{}')
+printenv
