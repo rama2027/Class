ebb8eac60984867043e9163a1618ebde5bc3f2ca
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-06 14:16:32 +0530

    Update Asssumerole.sh

--- Asssumerole.sh
+++ Asssumerole.sh
@@ -1,5 +1,5 @@
  #!/bin/sh
-aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance > ram1.txt
+aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance --output table > ram.txt
 cat ram1.txt
 key=awk '/AccessKeyId/ {print $2}' ram1.txt
 echo $key
