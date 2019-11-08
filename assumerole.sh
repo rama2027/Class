539ee460115ac454794bd9b5df09c020ef7b7ff0
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-08 18:44:20 +0530

    Update assumerole.sh

--- assumerole.sh
+++ assumerole.sh
@@ -2,7 +2,7 @@
 aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance --output json > Cred.json
 jq -r '.["Credentials"]' Cred.json > Cred.txt
 cat Cred.txt
-awk '/SecretAccessKey/ {print $2}' Cred.txt | tr -d '",{} > SecretAccessKey.txt
+awk '/SecretAccessKey/ {print $2}' Cred.txt | tr -d '",{}' > SecretAccessKey.txt
 awk '/SessionToken/ {print $2}' Cred.txt | tr -d '",{}' > SessionToken.txt
 awk '/AccessKeyId/ {print $2}' Cred.txt | tr -d '",{}' > AccessKeyId.txt
 cat AccessKeyId.txt
