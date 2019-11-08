2e782ef44c1819ac6a36244c2d2a74a93db01eb2
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-08 18:01:24 +0530

    Update assumerole.sh

--- assumerole.sh
+++ assumerole.sh
@@ -2,7 +2,13 @@
 aws sts assume-role --role-arn $IAM_ROLE --role-session-name DescribeInstance --output json > Cred.json
 jq -r '.["Credentials"]' Cred.json > Cred.txt
 cat Cred.txt
-export AWS_SECRET_ACCESS_KEY=$(awk '/SecretAccessKey/ {print $2}' Cred.txt | tr -d '",{}')
-export AWS_SESSION_TOKEN=$(awk '/SessionToken/ {print $2}' Cred.txt | tr -d '",{}')
-export AWS_ACCESS_KEY_ID=$(awk '/AccessKeyId/ {print $2}' Cred.txt | tr -d '",{}')
+awk '/SecretAccessKey/ {print $2}' Cred.txt | tr -d '",{} > SecretAccessKey.txt
+awk '/SessionToken/ {print $2}' Cred.txt | tr -d '",{}' > SessionToken.txt
+awk '/AccessKeyId/ {print $2}' Cred.txt | tr -d '",{}' > AccessKeyId.txt
+cat AccessKeyId.txt
+cat SessionToken.txt
+catSecretAccessKey.txt
+AWS_SECRET_ACCESS_KEY=$(awk '/SecretAccessKey/ {print $2}' Cred.txt | tr -d '",{}')
+AWS_SESSION_TOKEN=$(awk '/SessionToken/ {print $2}' Cred.txt | tr -d '",{}')
+AWS_ACCESS_KEY_ID=$(awk '/AccessKeyId/ {print $2}' Cred.txt | tr -d '",{}')
 printenv
