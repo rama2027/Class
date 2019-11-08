5e590400b9366e007307874303f9bff517678d13
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-08 18:16:37 +0530

    Update assumerole.sh

--- assumerole.sh
+++ assumerole.sh
@@ -7,7 +7,7 @@ awk '/SessionToken/ {print $2}' Cred.txt | tr -d '",{}' > SessionToken.txt
 awk '/AccessKeyId/ {print $2}' Cred.txt | tr -d '",{}' > AccessKeyId.txt
 cat AccessKeyId.txt
 cat SessionToken.txt
-catSecretAccessKey.txt
+cat SecretAccessKey.txt
 AWS_SECRET_ACCESS_KEY=$(awk '/SecretAccessKey/ {print $2}' Cred.txt | tr -d '",{}')
 AWS_SESSION_TOKEN=$(awk '/SessionToken/ {print $2}' Cred.txt | tr -d '",{}')
 AWS_ACCESS_KEY_ID=$(awk '/AccessKeyId/ {print $2}' Cred.txt | tr -d '",{}')
