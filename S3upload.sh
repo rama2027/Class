4315bacac8d56af14ccea811a675101b8d517ed7
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-17 15:28:17 +0530

    Update S3upload.sh

--- S3upload.sh
+++ S3upload.sh
@@ -1,2 +1,2 @@
 source $WORKSPACE/var.sh
-aws s3 cp $WORKSPACE/Create.yaml.yaml s3://cloudformationtest/create.yaml
+aws s3 cp $WORKSPACE/Create.yaml.yaml s3:///cf-templates-i6z3lkvgdo6f-eu-west-1/create.yaml
