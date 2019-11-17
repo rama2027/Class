6616024985bdf6102f5ac86be509beaa3e04ecce
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-17 15:28:45 +0530

    Update CTF.sh

--- CTF.sh
+++ CTF.sh
@@ -1,1 +1,2 @@
+source $WORKSPACE/var.sh
 aws cloudformation create-stack --stack-name myteststack --template-body file://s3.amazonaws.com//cf-templates-i6z3lkvgdo6f-eu-west-1/test.txt --parameters ParameterKey=KeyPairName,ParameterValue=TestKey ParameterKey=SubnetIDs,ParameterValue=SubnetID1\\,SubnetID2
