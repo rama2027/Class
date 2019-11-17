8de79fa22b4fe4680144954a9235155fc5410fe5
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-11-17 16:22:28 +0530

    Update CTF.sh

--- CTF.sh
+++ CTF.sh
@@ -1,2 +1,2 @@
 source $WORKSPACE/var.sh
-aws cloudformation create-stack --stack-name myteststack --template-body file:s3://cf-templates-i6z3lkvgdo6f-eu-west-1/create.yaml --region eu-west-1
+aws cloudformation create-stack --stack-name myteststack --template-url file:http://s3.amazonaws.com/cf-templates-i6z3lkvgdo6f-eu-west-1/create.yaml --region eu-west-1
