6f4855e544f25b37e106c3f9f731e4cd201b8923
rama2027 <37704469+rama2027@users.noreply.github.com>
2021-04-04 20:53:38 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,5 +3,5 @@
 source $WORKSPACE/var.sh
 export stackname=my-new-stack
 aws cloudformation create-stack --stack-name myteststack --template-url http://s3.amazonaws.com/cft-rama/cft.json --stack-name $stackname --region $Region
-aws cloudformation wait stack-create-complete --stack-name $stackname
+aws cloudformation wait stack-create-complete --stack-name $stackname --region $Region
 
