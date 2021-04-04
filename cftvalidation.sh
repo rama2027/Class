c727e95847238b9fdaa11f4329560dcfbab317e1
rama2027 <37704469+rama2027@users.noreply.github.com>
2021-04-04 17:33:10 +0530

    Update cftvalidation.sh

--- cftvalidation.sh
+++ cftvalidation.sh
@@ -4,11 +4,7 @@ source $WORKSPACE/var.sh
 validation=$(aws cloudformation validate-template --template-url https://s3.amazonaws.com/cft-rama/cft.json --region $Region)
 if [$? -eq 0]
 then
-{
 echo "validation is ok"
-}
 else
-{
 echo "validation is not ok"
-}
 fi
