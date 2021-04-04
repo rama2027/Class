f51b1e739a634800c25ece19ab8b5b4fe8a984f3
rama2027 <37704469+rama2027@users.noreply.github.com>
2021-04-04 17:21:41 +0530

    Update cftvalidation.sh

--- cftvalidation.sh
+++ cftvalidation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 source $WORKSPACE/var.sh
-validation=$(aws cloudformation validate-template --template-url https://s3.amazonaws.com/arn:aws:s3:::cft-rama/cft.json --region $Region)
+validation=$(aws cloudformation validate-template --template-url https://s3.amazonaws.com/cft-rama/cft.json --region $Region)
 if [ $? -eq 0]
 then
 {
