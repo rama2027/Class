42392f26ccd1fafe41e072fa1b7fad2628889beb
rama2027 <37704469+rama2027@users.noreply.github.com>
2021-04-10 12:39:28 +0530

    Update S3upload.sh

--- S3upload.sh
+++ S3upload.sh
@@ -1,4 +1,4 @@
 source $WORKSPACE/var.sh
-aws s3 cp $WORKSPACE/demo.json s3://cft-rama/cft.json
-aws s3 cp $WORKSPACE/iam.json s3://cft-rama/iam.json
+aws s3 cp $WORKSPACE/demo.json s3://cft-rama/$stackname.json
+aws s3 cp $WORKSPACE/iam.json s3://cft-rama/$stackname.json
 
