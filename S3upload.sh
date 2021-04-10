6065f4992af103249165d16f5506216aaf031872
rama2027 <37704469+rama2027@users.noreply.github.com>
2021-04-10 13:24:04 +0530

    Update S3upload.sh

--- S3upload.sh
+++ S3upload.sh
@@ -1,8 +1,10 @@
 source $WORKSPACE/var.sh
 if [[ $stackname == *"iam"* ]] || [[ $stackname == *"IAM"* ]]
 then
+echo "Uploading IAM Template"
 aws s3 cp $WORKSPACE/iam.json s3://cft-rama/$stackname.json
 else
+echo "Uploading CFT Teamplate"
 aws s3 cp $WORKSPACE/demo.json s3://cft-rama/$stackname.json
 fi
 
