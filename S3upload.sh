7782567039eb8184c375f4c4baf32f9dfcfdb5bd
rama2027 <37704469+rama2027@users.noreply.github.com>
2021-04-10 13:18:34 +0530

    Update S3upload.sh

--- S3upload.sh
+++ S3upload.sh
@@ -1,4 +1,8 @@
 source $WORKSPACE/var.sh
-aws s3 cp $WORKSPACE/demo.json s3://cft-rama/$stackname.json
+if [[ $stackname == *"iam"* ]] || [[ $stackname == *"IAM"* ]]
+then
 aws s3 cp $WORKSPACE/iam.json s3://cft-rama/$stackname.json
+else
+aws s3 cp $WORKSPACE/demo.json s3://cft-rama/$stackname.json
+fi
 
