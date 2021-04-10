d6fca1c7edfdfcb88a3940d03d70f250b6f3e3b0
rama2027 <37704469+rama2027@users.noreply.github.com>
2021-04-10 13:21:55 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -68,7 +68,7 @@ fi
 else
 echo " stack named $stackname exists hence checking the change set"
 echo "checking change set"
-aws cloudformation create-change-set --stack-name $stackname --change-set-name my-change-set --template-url http://s3.amazonaws.com/cft-rama/$stackname --change-set-type UPDATE --region $Region
+aws cloudformation create-change-set --stack-name $stackname --change-set-name my-change-set --template-url http://s3.amazonaws.com/cft-rama/$stackname.json --change-set-type UPDATE --region $Region
 aws cloudformation wait change-set-create-complete --stack-name $stackname --change-set-name my-change-set --region $Region
 changeid=$(aws cloudformation list-change-sets --stack-name $stackname --region $Region --output text)
 if [[ $changeid == *"didn't contain changes"* ]]
